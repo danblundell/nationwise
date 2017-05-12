ccb4efa75b8434d1c0feba096d50509e2195e36c
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2017-05-12 11:32:19 +0100

    extend sign up fields

--- Models/user.js
+++ Models/user.js
@@ -3,6 +3,9 @@ mongoose.Promise = global.Promise;
 
 var User = mongoose.model('Users', {
 	username: String,
+	forename: String,
+	surname: String,
+	email: String,
 	password: String,
 	updated: { type: Date, default: Date.now }
 	});
